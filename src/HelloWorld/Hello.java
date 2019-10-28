d30936f23517515af38b7e62ceae5ad15e93977f
simozmp <simo.zmp96@gmail.com>
2019-10-28 15:19:57 +0100

    Technical debt fixing

--- src/HelloWorld/Hello.java
+++ src/HelloWorld/Hello.java
@@ -1,10 +1,10 @@
-package HelloWorld;
+package helloworld;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
 public class Hello {
 	
-	private final static Logger LOGGER = Logger.getLogger(Hello.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(Hello.class.getName());
 	
 	public static void main(String args[]) {
 		LOGGER.log(Level.INFO, "Hello, World!");
